d26c4b959935dad8157f3cad4f9cb5b29c8c362a
Divanny <divannyjpm@gmail.com>
2023-04-30 01:32:45 -0400

    Making some changes on design

--- image-searcher/src/App.js
+++ image-searcher/src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
-import { Button } from 'primereact/button';
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-import "primereact/resources/primereact.min.css";    
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 
 function App() {
   return (
-    <div className="glass-container">
-      <div className="glass-card">
-        <h1 className="title">Image Searcher</h1>
-        <h2 className="subtitle">Find the perfect image for your project</h2>
-        <div className="search-container">
-          <input class="input" type="text" placeholder="Search..."/>
-          <Button icon="pi pi-search" rounded outlined aria-label="Search" />
+    <div>
+      <div className="glass-container">
+        <div className="glass-card">
+          <h1 className="title">Image Searcher</h1>
+          <h2 className="subtitle">Find the perfect <span className="transparent">image</span> for your project</h2>
+          <div className="search-container">
+            <input className="input" type="search" placeholder="Search..."/>
+            <button class="button">
+              <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" class="icon"/>
+            </button>
+          </div>
         </div>
+        <div className="area">
+            <ul className="circles">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+        </div >
       </div>
     </div>
   );
